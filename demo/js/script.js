e6d2dbd0c3c83cd2ce9d5298af49fcce5f10ad1f
Tekeden <adsevanssword@gmail.com>
2016-10-03 18:06:26 +0200

    Fix: Modification du script pour travailler avec un cube et ppas un import

--- demo/js/script.js
+++ demo/js/script.js
@@ -1,18 +1,18 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 0, 300)
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+var axisHelper = new THREE.AxisHelper(5); //Crée les axes x y z
+scene.add(axisHelper); //On ajoute les axes
 
-loader = new THREE.ColladaLoader();
-loader.load('modele/cube.dae', function colladaReady(collada) {
-    cube = collada.scene;
-    skin = collada.skins[0];
-    scene.add(cube);
-});
-
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+var material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 camera.position.z = 5;
 
 function render() {
