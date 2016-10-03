1ea79e98adba4da012960c563603829a17eacdf7
Enzo-M <enzomena@gmail.com>
2016-10-03 22:08:50 +0200

    Fix : Taille de la fenêtre

--- demo/js/script.js
+++ demo/js/script.js
@@ -3,11 +3,11 @@
  animate();
 
  function init() {
-     camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+     camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000); //init de la camera
      camera.position.z = 500;
 
      controls = new THREE.TrackballControls(camera);
-     controls.addEventListener('change', render);
+     controls.addEventListener('change', render); 
      controls.rotateSpeed = 5.0;
      controls.zoomSpeed = 1.2;
      controls.panSpeed = 0.8;
@@ -18,23 +18,33 @@
      controls.keys = [65, 83, 68];
      scene = new THREE.Scene();
 
-     var lightAmb = new THREE.AmbientLight(0xffffff);
+     var lightAmb = new THREE.AmbientLight(0xffffff); //light
      scene.add(lightAmb);
 
-     var geometry = new THREE.CubeGeometry(100, 100, 100);
-     var material = new THREE.MeshNormalMaterial();
-     var cube = new THREE.Mesh(geometry, material);
-     scene.add(cube);
+     var geometry = new THREE.CubeGeometry(100, 100, 100); // forme 
+     var material = new THREE.MeshNormalMaterial();  //material
+     var cube = new THREE.Mesh(geometry, material); //init cube
+     scene.add(cube); //ajout du cube à la scène
      renderer = new THREE.WebGLRenderer();
      renderer.setSize(window.innerWidth, window.innerHeight)
-     document.body.appendChild(renderer.domElement);
+     document.body.appendChild(renderer.domElement); // permet d'intégrer la fenêtre de rendu dans la page HTML
+
+     window.addEventListener('resize', onWindowResize, false); //event => ajuste la taille de la fenêtre
  }
 
  function animate() {
-     requestAnimationFrame(animate);
+     requestAnimationFrame(animate); 
      controls.update();
  }
 
  function render() {
-     renderer.render(scene, camera);
+     renderer.render(scene, camera); 
+ }
+
+ function onWindowResize() { // fonction pour ajuster la scène à la taille de la fenêtre
+     camera.aspect = window.innerWidth / window.innerHeight;
+     camera.updateProjectionMatrix();
+     renderer.setSize(window.innerWidth, window.innerHeight);
+     controls.handleResize();
+     render();
  }
