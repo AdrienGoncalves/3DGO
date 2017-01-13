67924a55731925a17faf5ddab86b4a61790ca1fb
Le Goff Maël <legoffmael@gmail.com>
2017-01-13 16:39:03 +0100

    Add files via upload
    Les boutons Tutoriel et A Propos sont désormais fonctionnels

---
+++ 3DGO v1.1 - tuto et contact/3d_rendering.js
@@ -0,0 +1,290 @@
+/**
+ * Script javascript qui contiendra les initialisations des caméras et des élements 3D
+ * @author Maël Le Goff
+ */
+	var container;
+	var width;
+	var height;
+	
+	var camera, scene, controls, renderer;
+	
+	var plane; // le sol
+	
+	var clock = new THREE.Clock();
+	
+	var raycaster = new THREE.Raycaster();
+	var projector = new THREE.Projector();
+	var directionVector = new THREE.Vector3();
+	
+	var generalInfosNode;
+	var vueInfosNode;
+	
+	var marker;
+	
+	var result;
+	$.ajax( { //Création de l'objet AJAX
+            type: "GET",
+            async: false,
+            url: 'collada/collada.dae',
+            dataType: "xml",
+            success: function(data) {
+              result = data;
+            }
+        });
+		
+	var	right = new THREE.Vector3();
+	var	up = new THREE.Vector3();
+	var	at = new THREE.Vector3();
+	//////////////////////////////////////////////////////////////////////////////////////
+
+	initContainer();
+	document.onload = initInformations();
+	initCameraScene();
+	initLights();
+	
+	initControls();
+	
+	initGround();
+	initCollada();
+	initMarker();
+	
+	initRenderer();
+	
+	animate();
+	
+	//////////////////////////////////////////////////////////////////////////////////////
+	
+	/*On determine le conteneur du rendu et sa taille*/
+	function initContainer() {	
+		/*container = document.createElement('div');
+		container.setAttribute("id", "rendererArea");
+		document.body.appendChild(container);*/
+		container=document.getElementById('dessein');
+		//document.body.appendChild(container);
+		width = container.clientWidth;
+		height = container.clientHeight;
+	}
+	
+	/*On determine les zone ou afficher les informations*/
+	function initInformations() {	
+		generalInfosNode = document.getElementById('nav1'); //Le menu "Informations général"
+		vueInfosNode = document.getElementById('nav2'); //Le menu "Informations sur la vue");
+	}
+	
+	/*Initialisation de la scène et d'une camera*/
+	function initCameraScene() {		
+		scene = new THREE.Scene();
+		scene.background = new THREE.Color( 0x808080 ); //couleur de fond du rendu
+		
+		camera = new THREE.PerspectiveCamera( 48.8, width / height, 1, 10000 ); //Caractéristiques de la caméra
+		camera.position.set(185,185,120); //Position de la caméra
+		scene.add(camera);
+	}
+
+	/** Permet de changer les caractéristiques de la caméra*/
+	function changeCam(x1, y1, z1, x2, y2, z2){
+		camera.position.set(x1,y1,z1); //Position de la caméra
+		controls.target = (new THREE.Vector3(x2,y2,z2)); //Orientation de la caméra
+	}
+	
+	/*Initialisation des lumières */
+	function initLights() {	
+		var ambientLight = new THREE.AmbientLight(0x606060);
+        scene.add(ambientLight);
+		
+        var directionalLight = new THREE.DirectionalLight(0xffffff);
+        directionalLight.position.set(1, 0.75, 0.5).normalize();
+        scene.add(directionalLight);
+	}
+	
+	/**Création d'un controleur de type trackball qui permettra d'effectuer
+     * des roation ainsi que des zooms à différente vitesses
+     */
+	function initControls() {	
+		controls = new THREE.TrackballControls( camera,container );	
+			controls.rotateSpeed = 2.0;
+			controls.zoomSpeed = 1.2;
+			controls.panSpeed = 0.8;
+			controls.noZoom = false;
+			controls.noPan = true; //pour que le clic droit ne serve à rien pour le trackball
+			controls.staticMoving = true;
+			controls.dynamicDampingFactor = 0.3;
+			controls.keys = [65, 83, 68];
+			controls.target = (new THREE.Vector3(0,0,0)); //Orientation de la caméra
+		controls.addEventListener('change', render);
+	}
+	
+	/*Initialisation d'un sol pour le rendu*/
+	function initGround() {
+		plane = new THREE.Mesh(new THREE.PlaneGeometry(1000, 1000, 10, 10), new THREE.MeshBasicMaterial({ color: 0x000000, wireframe: true }));
+        plane.name = 'Sol';
+		plane.position.set(0,0,0);
+		plane.rotation.x = 1.57;
+		plane.name = 'Sol';
+        scene.add(plane);
+	}
+	
+	/* Charger un fichier Collada */
+	function initCollada() {
+		var loader = new THREE.ColladaLoader();
+		loader.options.convertUpAxis = true;
+		loader.load( 'collada/collada.dae', function ( collada ) {
+			
+			var dae = collada.scene;
+			
+			dae.position = plane.position //on le pose sur le sol
+			dae.scale.set(1.5,1.5,1.5);
+
+			scene.add(dae);
+		});
+	}
+	
+	/*Definition du marqueur lorsqu'on clique*/
+	function initMarker() {
+		marker = new THREE.Mesh( new THREE.SphereGeometry(1), new THREE.MeshLambertMaterial( { color: 0xff0000 } ) );
+        scene.add(marker);
+	}
+	
+	/*Initialisation du rendu*/
+	function initRenderer() {
+		//Ajouter des aides : les axes et une grille
+		// var axes = new THREE.AxisHelper(50);
+		// axes.position.set(0,0,0);
+		// scene.add(axes);
+		// var gridXZ = new THREE.GridHelper(100, 10);
+		// gridXZ.position.set(0,0,0 );
+		// scene.add(gridXZ);
+		
+		//Rendu pour collada
+		renderer = new THREE.WebGLRenderer( { antialias: false } );
+			renderer.setPixelRatio( window.devicePixelRatio );
+			renderer.setSize( width, height );
+		container.appendChild( renderer.domElement );
+		
+		//Evenement lorsque que l'on clique sur le click droit de la souris dans le container
+		container.addEventListener( 'contextmenu', onDocumentRightClick, false );
+
+		window.addEventListener( 'resize', onWindowResize, false );
+	}
+	
+	//////////////////////////////////////////////////////////////////////////////////////
+
+
+	/*Fonction qui redimensionne la scene quand on redimensionne la fenêtre */
+	function onWindowResize() {
+		width = container.clientWidth; //pour que le menu de droite reste affiché
+		camera.aspect = width / height;
+		camera.updateProjectionMatrix();
+		
+		renderer.setSize( width, height );
+	}
+
+	/*Fonction qui ajoute une particule à l'endroit cliqué*/
+	function onDocumentRightClick( event ) {
+		event.preventDefault();
+		
+		generalInfosNode.innerHTML = '';
+		vueInfosNode.innerHTML = '';
+		
+		//Definition de la zone ou l'utilisateur a cliqué
+		var x = ( (event.clientX-container.offsetLeft)/ width ) * 2 - 1;
+		var y = - ( event.clientY / height ) * 2 + 1;
+		
+		//Définition de notre vecteur de direction à ces valeurs initiales
+		directionVector.set(x, y, 1);
+
+		//Ne pas projeter
+		projector.unprojectVector(directionVector, camera);
+
+		//Soustraire le vecteur représentant la position de la caméra
+		directionVector.sub(camera.position);
+
+		//Permet de normalisé le vecteur, pour éviter de grands nombres de la projection et de la soustraction
+		directionVector.normalize();
+
+		// On initialise le lancer de rayon (raycaster)
+		raycaster.set(camera.position, directionVector);
+
+		// On appel le lancer de rayon
+		// (Le second argument signifie "recursif")
+		var intersects = raycaster.intersectObjects(scene.children, true);
+
+		if (intersects.length>0) {
+			//On positionne le marqueur à l'endroit cliqué
+			marker.position.x = intersects[0].point.x;
+			marker.position.y = intersects[0].point.y;
+			marker.position.z = intersects[0].point.z;
+			
+			//On affiche les informations
+			displayInformations(intersects);
+		}
+	}
+	
+	/*Fonction qui affiche les informations spécifiques à l'élément du fichier séléctionné dans les menus*/
+	function displayInformations(intersects) {
+
+		/*Parcourir le  fichier collada à la recherche de la balise création*/
+		var creation_temp = '';
+		var creation = '';
+		var creation_temp = $(result).find('created').text();
+		if (creation_temp != '') {
+			creation = creation_temp.replace("T", " à ");
+		}
+		
+		var target = intersects[0].object;
+		generalInfosNode.innerHTML = '<h2>ID: ' + target.id + '</h2>' +
+									 '<h2>Date de création: ' + creation + '</h2>';
+	}
+	
+	/*Fonction appelée depuis l'interface qui permet de zoomer*/
+	function zoomIn() {
+		camera.matrix.extractBasis(right,up,at);
+        camera.position.add(at.multiplyScalar(-5));
+        camera.matrix.extractBasis(right,up,at);
+	}
+	
+	/*Fonction appelée depuis l'interface qui permet de dézoomer*/
+	function zoomOut() {
+		camera.matrix.extractBasis(right,up,at);
+        camera.position.add(at.multiplyScalar(+5));
+        camera.matrix.extractBasis(right,up,at); 
+	}
+
+	/*Fonction appelée depuis l'interface qui permet de faire une rotation vers le haut*/
+	function moveUp() {
+		camera.matrix.extractBasis(right,up,at);
+		camera.position.add(up.multiplyScalar(5));
+        camera.matrix.extractBasis(right,up,at);  
+	}
+	
+	/*Fonction appelée depuis l'interface qui permet de faire une rotation vers la gauche*/
+	function moveLeft() {
+		camera.matrix.extractBasis(right,up,at);
+        camera.position.add(right.multiplyScalar(-5));
+        camera.matrix.extractBasis(right,up,at); 
+	}
+
+	/*Fonction appelée depuis l'interface qui permet de faire une rotation vers la droite*/
+	function moveRight() {
+		camera.matrix.extractBasis(right,up,at);
+		camera.position.add(right.multiplyScalar(5));
+        camera.matrix.extractBasis(right,up,at); 
+	}
+
+	/*Fonction appelée depuis l'interface qui permet de faire une rotation vers le bas*/
+	function moveDown() {
+		camera.matrix.extractBasis(right,up,at);
+		camera.position.add(up.multiplyScalar(-5));
+        camera.matrix.extractBasis(right,up,at); 
+	}
+
+	function animate(){
+		var delta = clock.getDelta();
+		requestAnimationFrame( animate );
+		controls.update();
+		render(delta);
+	}
+
+	function render(delta){
+		renderer.render( scene, camera );
+	}
---
+++ 3DGO v1.1 - tuto et contact/index.php
@@ -0,0 +1,276 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <title>3D GO</title>
+    <meta charset="utf-8">
+	<link rel="stylesheet" href="stylesheet.css"/>
+    <link rel="icon" type="image/ico" href="Images/logo_le2i.png"/>
+	
+	<!--Bibliothèque principal-->
+    <script src="js/Three.js"></script>
+	<!--Bibliothèques basés sur Three.js-->
+	<script src="js/CanvasRenderer.js"></script>
+    <script src="js/ColladaLoader.js"></script>
+	<script src="js/Projector.js"></script>
+    <script src="js/TrackballControls.js"></script>
+	<script src="js/Stats.js"></script>
+	<!--Nos fichiers javascript-->
+	<script src="[grp1]interface.js"></script>
+	<script src="[grp2]vues.js"></script>
+	<script src="[grp3]graphes.js"></script>
+	<script src="js/graph.js"></script>
+	<script src="js/GrapheManager.js"></script>
+	<script src="js/porte.js"></script>
+	<script src="js/jquerry.js"></script>
+	<script src="3d_rendering.js"></script>
+</head>
+
+<body>
+
+	<div class="menuGauche">
+		<div class="menu1" id="menu1">
+			<nav>
+				<h1>Déplacement</h1>
+				
+				<div id="leftImage">
+					<img src="Images/zoom_in.png" width="50" height="50" style="cursor: pointer;" onclick="zoomIn()"/><br/>
+					<img src="Images/zoom_out.png" width="50" height="50" style="cursor: pointer;" onclick="zoomOut()"/>
+				</div>
+				<div id="rightImage">
+					<img src="Images/directional_arrows.png" width="100" height="100" alt="move" usemap="#direction" />
+						<map name="direction">
+						  <area shape="rect" coords="65,0,35,38" onClick="moveUp()" alt="UP"/>
+						  <area shape="rect" coords="38,35,0,65" onClick="moveLeft()" alt="LEFT" />
+						  <area shape="rect" coords="62,35,100,65" onClick="moveRight()" alt="RIGHT"/>
+						  <area shape="rect" coords="65,100,35,62" onClick="moveDown()" alt="DOWN"/>
+						</map>
+				</div>
+				
+				<div id="boutons">
+
+					<label><input type="checkBox" name="vueOne" value="vueCheckOne" onclick="changeVue()" id="vueOne"/> Activer la vue caméra </label>
+
+					<div id="listOne"  style="display:none;">
+						<select name="list1" id="list1">
+							   <option>Caméra 1</option>
+							   <option>Caméra 2</option>
+							   <option>Caméra 3</option>
+							   <option>Caméra 4</option>
+							</select>
+						<br />					
+						<script>
+							var list = document.getElementById('list1');
+							list.addEventListener('change', function() {						
+								// On envoi 1 et le contenu de l'élément <option> ciblé par la propriété selectedIndex
+								vueSelection(1,list.options[list.selectedIndex].innerHTML);
+							}, true);
+						</script>						
+					</div>
+					
+					<div id="checkTwo"  style="display:none;">
+						<br/>
+						<label><input type="checkBox" name="vueTwo" value="vueCheckTwo" onclick="changeVue()" id="vueTwo"/> Activer la vue caméra </label>
+
+						<div id="listTwo"  style="display:none;">
+						<select name="list2" id="list2">
+							   <option>Caméra 1</option>
+							   <option>Caméra 2</option>
+							   <option>Caméra 3</option>
+							   <option>Caméra 4</option>
+							</select>
+						</div>
+						<br />
+						<script>
+							var list2 = document.getElementById('list2');
+							list2.addEventListener('change', function() {						
+								// On envoi 2 et le contenu de l'élément <option> ciblé par la propriété selectedIndex
+								vueSelection(2,list2.options[list2.selectedIndex].innerHTML);
+							}, true);
+						</script>
+					</div>
+
+					<div id="checkThree"  style="display:none;">
+						<br/>
+						<label><input type="checkBox" name="vueThree" value="vueCheckThree" onclick="changeVue()" id="vueThree"/> Activer la vue caméra </label>
+
+						<div id="listThree"  style="display:none;">
+						<select name="list3" id="list3">
+							   <option>Caméra 1</option>
+							   <option>Caméra 2</option>
+							   <option>Caméra 3</option>
+							   <option>Caméra 4</option>
+							</select>
+						</div>
+						<br />
+						<script>
+							var list3 = document.getElementById('list3');
+							list3.addEventListener('change', function() {						
+								// On envoi 1 et le contenu de l'élément <option> ciblé par la propriété selectedIndex
+								vueSelection(3,list3.options[list3.selectedIndex].innerHTML);
+							}, true);
+						</script>
+					</div>
+					
+					<br/><br/>
+					<!-- Importation du fichier collada-->
+					<form method="post" action="index.php" enctype="multipart/form-data">
+						 <label for="fichier_collada">Fichier Collada (.dae) :</label><br />
+						 <input type="file" name="fichier_collada" id="fichier_collada" accept=".dae"/><br />
+						 <input type="submit" name="submit_collada" value="Envoyer" />
+					</form>
+					<?php
+						if(isset($_POST['submit_collada']))
+						{	
+							//Vérifier si bien uploadé
+							if ($_FILES['fichier_collada']['error'] > 0) $erreur = 'Erreur lors du transfert Collada<br/>';
+							else
+							{
+								//Vérifier le format du fichier
+								$extensions_valides = array( 'dae' ); //liste des formats correctes
+								$extension_upload = strtolower(  substr(  strrchr($_FILES['fichier_collada']['name'], '.')  ,1)  );
+								if ( in_array($extension_upload,$extensions_valides) )
+								{
+									//Déplacer le fichier
+									$nom = "collada/collada.dae";
+									$resultat = move_uploaded_file($_FILES['fichier_collada']['tmp_name'],$nom);
+									if ($resultat) echo "Transfert réussi (Si rien ne s'affiche il se peut que votre fichier soit non lisible)";
+									else echo "Problème lors du tranfert";
+								}
+								else echo "Le format du fichier doit être un .dae";
+							}
+						}
+					?>
+					<br />
+					<input type="button" onclick="resetCamera()" value="Réinitialiser la vue"/></input>
+				</div>
+
+		</div>
+		<div class="menu2" id="menu2">
+			<nav>
+				<h1>Actions</h1>
+				<br/>
+				<!-- Importation du fichier XML-->
+				<form method="post" action="index.php" enctype="multipart/form-data">
+					 <label for="fichier_xml">Fichier XML (.xml) :</label><br />
+					 <input type="file" name="fichier_xml" id="fichier_xml" accept=".xml"/><br />
+					 <input type="submit" name="submit_xml" value="Envoyer" />
+				</form>
+				<?php
+					if(isset($_POST['submit_xml']))
+					{	
+						//Vérifier si bien uploadé
+						if ($_FILES['fichier_xml']['error'] > 0) $erreur = 'Erreur lors du transfert<br/>';
+						
+						//Vérifier le format du fichier
+						$extensions_valides = array( 'xml' ); //liste des formats correctes
+						$extension_upload = strtolower(  substr(  strrchr($_FILES['fichier_xml']['name'], '.')  ,1)  );
+						if ( in_array($extension_upload,$extensions_valides) ) '<script>alert.log("Extension correcte")';
+						
+						//Déplacer le fichier
+						$nom = "xml/testPorte.xml";
+						$resultat = move_uploaded_file($_FILES['fichier_xml']['tmp_name'],$nom);
+						if ($resultat) '<script>alert.log("Transfert réussi")';
+					}
+				?>
+				
+				<div id="boutons" style="margin-top:15px;">
+					<input type="button" onclick="createDoors()" value="Créer les portes"/></input>
+				</div>
+				
+				<div id="boutons" style="margin-top:15px;">
+					<label for="fichier_xml">Id Porte 1 :</label>
+					<input type="text" name="door1" id="door1"><br>
+					<label for="fichier_xml">Id Porte 2 :</label>
+					<input type="text" name="door2" id="door2"><br>
+					<input type="button" onclick="createRoad()" value="Générer chemin"/></input>
+				</div>
+				
+				<div class="lienModal" style="left:10px">
+					<a href="#modal_tuto">Tutoriel</a>
+				</div>
+				<div class="lienModal" style="right:10px;">
+					<a href="#modal_contact">À propos</a>
+				</div>
+			</nav>
+		</div>
+	</div>
+	
+	<!--Fenêtre modale du tuto-->
+	<div id="modal_tuto" class="modalDialog">
+		<div>	
+			<a href="#close" title="Fermer" class="close">X</a>
+			<h3>Tutoriel</h3>
+			<p>Bienvenue dans 3DGO !</p>
+			<p>3DGO vous permet d'afficher vos fichiers COLLADA.</p>
+			<p>Pour cela rien de plus simple : cliquez sur "Choississez un fichier" et séléctionnez votre fichier COLLADA puis cliquer sur "Envoyer". L'application va ensuite importer le fichier puis va l'afficher.</p>
+			<p>Pour intéragir avec le fichier :</p>
+			<ul>
+				<li><u>Pour tourner autour de l'objet :</u> Clique gauche et déplacer la souris ou vous pouvez utiliser les flêches de l'interface.</li>
+				<li><u>Pour zoomer/dézoomer :</u> Si vous êtes sur mozilla firefox vous pouvez utiliser la molette de la souris, sinon vous pouvez utiliser les loupes de l'interface.</li>
+				<li><u>Pour afficher des informations sur l'objet :</u> Vous pouvez faire une clique droit sur le fichier.</li>
+			</ul>
+			<p>Pour afficher les caméras :</p>
+			<ul>
+				<li>Cocher "Activer la vue caméra" puis choississez la caméra que vous souhaitez afficher.</li>
+				<li>Répéter l'action pour le nombre de caméra que vous voulez afficher (4 maximum).</li>
+			</ul>
+			<p>Pour afficher le plus court chemin entre 2 portes :</p>
+			<p>Pour cela il faut importer le fichier XML associé à votre fichier COLLADA.</p>
+			<ul>
+				<li>Cliquer sur "Choississez un fichier" puis choississez le fichier XML associé puis cliquer sur "Envoyer".</li>
+				<li>Cliquer sur "Créer les portes" pour générer les portes du fichier, une fenêtre va afficher la liste des portes générés ainsi que leurs coordonnées.</li>
+				<li>Entrer les numéros des 2 portes dont vous souhaitez connaitre le plus courts chemin puis faites générer chemin.</li>
+				<li>Une nouvelle fenêtre affichant le plus court chemin est apparu.</li>
+			</ul>
+			<p>Bonne utilisation !</p>
+		</div>
+	</div>
+	
+	<!--Fenêtre modale de contact-->
+	<div id="modal_contact" class="modalDialog">
+		<div>	
+			<a href="#close" title="Fermer" class="close">X</a>
+			<h3>À propos</h3>
+			<p>3DGO à été développée par : <a href="https://www.linkedin.com/in/dylan-barquilla-5aa512b3" target="_blank">Barquilla Dylan</a>, <a href="https://www.linkedin.com/in/elo%C3%AFse-grillet-1b5884133" target="_blank">Grillet Éloïse</a>, <a href="https://www.linkedin.com/in/adrien-goncalves-08ba70120" target="_blank">Goncalves Adrien</a>, <a href="https://www.linkedin.com/in/gautier-kasperek-086884133" target="_blank">Kasperek Gautier</a>, <a href="https://www.linkedin.com/in/lucas-lamarque-820884133" target="_blank">Lamarque Lucas</a>, <a href="https://www.linkedin.com/in/legoffmael" target="_blank">Le Goff Maël</a> et <a href="https://www.linkedin.com/in/enzo-menassol-3b31a1136" target="_blank">Mennassol Enzo</a>;
+			tous étudiants de 2ème année à l'IUT informatique de Dijon (2016/2017), dans le cadre de notre projet de 2ème année.</p>
+			<p>Nous avons été encadrés par <a href="https://www.linkedin.com/in/arnaudrolet" target="_blank">Rolet Arnaud</a> et <a href="https://www.linkedin.com/in/christophenicolle" target="_blank">Nicolle Christophe</a>, appartenants au laboratoire <a href="https://checksem.u-bourgogne.fr/#/home" target="_blank">Checksem</a>.</p>
+		</div>
+	</div>
+
+	<div class="centre" id="dessein">
+		
+	</div>
+	
+	<div class="menuDroite">
+		<div class="menu3" id="menu3">
+			<nav>		
+				<h1>Informations général</h1>
+				<nav id="nav1">		
+				
+				
+				
+			</nav>
+				
+				
+			</nav>	
+		</div>
+		<div class="menu4" id="menu4">
+			<nav>
+				<h1>Informations sur la vue</h1>
+				
+				<nav id="nav2">		
+				
+				
+				
+			</nav>
+
+			</nav>
+		</div>
+	</div>
+	
+	
+	<script src="3d_rendering.js"></script>
+
+</body>
+</html>
